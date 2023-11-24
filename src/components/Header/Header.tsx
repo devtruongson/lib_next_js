fd132ddc357ff6b11a09cc10151b7664bad03208
devtruongson <truongsonpt.80@gmail.com>
2023-11-24 10:35:17 +0700

    update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -227,7 +227,7 @@ const Header: React.FC = () => {
                                     return (
                                         <div className="py-6" key={uuid4()}>
                                             <Link
-                                                href="/auth/login"
+                                                href={item.href}
                                                 className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                             >
                                                 {item.name}
